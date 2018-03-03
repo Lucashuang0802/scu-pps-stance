da6cb5743929355786d1dfd44daa80cabe409a1b
Lucas Huang <xhuang0802@gmail.com>
2018-03-02 16:18:48 -0800

    pre-generate features

--- ext_feature_eng.py
+++ ext_feature_eng.py
@@ -1,3 +1,27 @@
 from feature_engineering import *
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.tokenize import sent_tokenize
 
+def tf_idf_features(headlines, bodies):
+    X = []
+    for _, (headline, body) in tqdm(enumerate(zip(headlines, bodies))):
+        clean_bodies = []
+        sentences = sent_tokenize(body)
 
+        for sentence in sentences:
+            clean_body = clean(sentence)
+            clean_bodies.append(" ".join(get_tokenized_lemmas(clean_body)))
+
+        clean_headline = clean(headline)
+        clean_headline = get_tokenized_lemmas(clean_headline)
+        clean_headline = " ".join(clean_headline)   
+
+        tfidf_vectorizer = TfidfVectorizer(min_df = 1)
+        tfidf_matrix = tfidf_vectorizer.fit_transform(clean_bodies + [clean_headline])
+        cosine = cosine_similarity(tfidf_matrix[len(clean_bodies)], tfidf_matrix) 
+        the_cosine = cosine[0]
+        winner = max(the_cosine[:-1])
+        X.append([winner])
+
+    return X
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import numpy as np
+import numpy as np
+
+from sklearn.ensemble import GradientBoostingClassifier
 from feature_engineering import refuting_features, polarity_features, hand_features, gen_or_load_feats
 from feature_engineering import word_overlap_features
-from utils.dataset import DataSet
+from ext_feature_eng import tf_idf_features
 
-LABELS = ['agree', 'disagree', 'discuss', 'unrelated']
-LABELS_RELATED = ['unrelated','related']
-RELATED = LABELS[0:3]
+from utils.dataset import DataSet
+from utils.generate_test_splits import kfold_split, get_stances_for_folds
+from utils.score import report_score, LABELS, score_submission
+from utils.system import parse_params, check_version
 
 def generate_features(stances,dataset,name):
     h, b, y = [],[],[]
@@ -20,11 +24,77 @@ def generate_features(stances,dataset,name):
     X_polarity = gen_or_load_feats(polarity_features, h, b, "features/polarity."+name+".npy")
     X_hand = gen_or_load_feats(hand_features, h, b, "features/hand."+name+".npy")
 
-    X = np.c_[X_hand, X_polarity, X_refuting, X_overlap]
+    X_tf_idf = gen_or_load_feats(tf_idf_features, h, b, "features/tf_idf."+name+".npy")
+
+    X = np.c_[X_hand, X_polarity, X_refuting, X_overlap, X_tf_idf]
     return X,y
 
-# Load the competition dataset
-competition_dataset = DataSet("competition_test")
-X_competition, y_competition = generate_features(competition_dataset.stances, competition_dataset, "competition")
-print(X_competition)
-print(y_competition)
+if __name__ == "__main__":
+    check_version()
+    parse_params()
+
+    #Load the training dataset and generate folds
+    d = DataSet()
+    folds,hold_out = kfold_split(d,n_folds=10)
+    fold_stances, hold_out_stances = get_stances_for_folds(d,folds,hold_out)
+
+    # Load the competition dataset
+    competition_dataset = DataSet("competition_test")
+    X_competition, y_competition = generate_features(competition_dataset.stances, competition_dataset, "competition")
+
+    Xs = dict()
+    ys = dict()
+
+    # Load/Precompute all features now
+    X_holdout,y_holdout = generate_features(hold_out_stances,d,"holdout")
+    for fold in fold_stances:
+        Xs[fold],ys[fold] = generate_features(fold_stances[fold],d,str(fold))
+
+
+    best_score = 0
+    best_fold = None
+
+
+    # Classifier for each fold
+    for fold in fold_stances:
+        ids = list(range(len(folds)))
+        del ids[fold]
+
+        X_train = np.vstack(tuple([Xs[i] for i in ids]))
+        y_train = np.hstack(tuple([ys[i] for i in ids]))
+
+        X_test = Xs[fold]
+        y_test = ys[fold]
+
+        clf = GradientBoostingClassifier(n_estimators=200, random_state=14128, verbose=True)
+        clf.fit(X_train, y_train)
+
+        predicted = [LABELS[int(a)] for a in clf.predict(X_test)]
+        actual = [LABELS[int(a)] for a in y_test]
+
+        fold_score, _ = score_submission(actual, predicted)
+        max_fold_score, _ = score_submission(actual, actual)
+
+        score = fold_score/max_fold_score
+
+        print("Score for fold "+ str(fold) + " was - " + str(score))
+        if score > best_score:
+            best_score = score
+            best_fold = clf
+
+
+    #Run on Holdout set and report the final score on the holdout set
+    predicted = [LABELS[int(a)] for a in best_fold.predict(X_holdout)]
+    actual = [LABELS[int(a)] for a in y_holdout]
+
+    print("Scores on the dev set")
+    report_score(actual,predicted)
+    print("")
+    print("")
+
+    #Run on competition dataset
+    predicted = [LABELS[int(a)] for a in best_fold.predict(X_competition)]
+    actual = [LABELS[int(a)] for a in y_competition]
+
+    print("Scores on the test set")
+    report_score(actual,predicted)
